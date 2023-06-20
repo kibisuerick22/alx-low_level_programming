cf0c94514dae1d59c2745abd6a072721f83e632c
kibisuerick22 <kibisuerick20@gmail.com>
2023-06-20 15:51:20 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+/**
+ * jack_bauer -  a function that prints every minute of the day of Jack Bauer.
+ * Return: ...
+ */
+
+int jack_bauer(void)
+
+{
+	int j, k, l, m;
+
+	for (j = 0; j <= 2; j++)
+	{
+	for (k = 0; k <= 9; k++)
+	{
+	if ((j <= 1 && k <= 9) || (j <= 2 && k <= 3))
+	{
+	for (l = 0; l <= 5; l++)
+	{
+	for (m = 0; m <= 9; m++)
+	{
+	_putchar(j + '0');
+	_putchar(k + '0');
+	_putachar(58);
+	_putchar(l + '0');
+	_putchar(m + '0');
+	_putchar('\n');
+	}
+	}
+	}
+	}
+	}
+}
