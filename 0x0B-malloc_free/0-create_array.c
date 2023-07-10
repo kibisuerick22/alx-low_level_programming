3fde9ff75e63cd2006cf694b5de458721e8cfe1d
kibisuerick22 <kibisuerick20@gmail.com>
2023-07-10 14:17:10 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,10 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
-
-
 /**
- * create_array - A function that creates an array of chars.
- * * @size: size of array
+ * create_array - create array of size size and assign char c
+ * @size: size of array
  * @c: char to assign
  * Description: create array of size size and assign char c
  * Return: pointer to array, NULL if fail
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,3 @@
-main.h
-
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -9,7 +7,7 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
+char **strtow
 
 #endif
 
